de834970573094e4404c7e50c64d8fad3f2aeb5f
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-11-01 12:58:35 +0900

    Derivative Add Uy Part.

--- src/derivative.jl
+++ src/derivative.jl
@@ -1,3 +1,5 @@
+# Derivative.
+#
 
 bond_trg_derivative(Ux0_s::Array{ElType, 3}, ∂Ux0_s::Array{ElType, 3}, # << This ∂ is imposed on S_innery Ux0. Not just Ux0.
                     Ux1_s::Array{ElType, 3}, ∂Ux1_s::Array{ElType, 3},
@@ -89,45 +91,90 @@ bond_trg_derivative(Ux0_s::Array{ElType, 3}, ∂Ux0_s::Array{ElType, 3}, # << Th
     spmm(A, B) = Array(A * B)
 
     # Ux-part, diagonal part.
-    Ux0_2isom = reshape(Ux0_2isom, (χ2^2, χc))
-    Ux1_2isom = reshape(Ux1_2isom, (χ2^2, χc))
-    UdAVx = Ux0_2isom'spmm(∂Tx, Ux1_2isom)
-    ∂Sx = diag(UdAVx)
-    Fij = [if i == j
-               0.0
-           else
-               1.0 / (Sx_2[j]^2 - Sx_2[i]^2)
-           end for i=1:χc, j=1:χc]
-    UdAVSx = UdAVx * Diagonal(Sx_2)
-    SUdAVx = Diagonal(Sx_2) * UdAVx
-    ∂Ux0_2iso = Ux0_2isom * (Fij .*(UdAVSx + UdAVSx'))
-    ∂Ux1_2iso = Ux1_2isom * (Fij .*(SUdAVx + SUdAVx'))
-    US2x = Ux0_2isom * Diagonal(Sx_2.^2)
-    VS2x = Ux1_2isom * Diagonal(Sx_2.^2)
-    # U off-diagonal part.
-    # LHS
-    ∂LHS_ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(Sx_2) + spmm(Tx, spmm(∂Tx', Ux0_2isom))
-    ∂LHS_ΓUx -= Ux0_2isom * (UdAVSx + UdAVSx')
-    # Trial: solution at U⊥'dAV⊥ = 0.
-    ∂ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(1.0 ./Sx_2) - Ux0_2isom * UdAVx * Diagonal(1.0 ./Sx_2)
-    _, convUx = cg!(vec(∂ΓUx),
-                    LinearMap{ElType}(∂v -> begin
-                                          ∂Γ = reshape(∂v, (χ2^2, χc))
-                                          vec(∂Γ*Diagonal(Sx_2.^2) - spmm(Tx, spmm(Tx', ∂Γ)) + US2x*Ux0_2isom'∂Γ)
-                                      end, nothing, χ2^2*χc, χ2^2*χc),
-                    vec(∂LHS_ΓUx), log=true)
-    ∂Ux0_2iso += ∂ΓUx
-    # V off-diagonal part.
-    ∂LHS_ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(Sx_2) + spmm(Tx', spmm(∂Tx, Ux1_2isom))
-    ∂LHS_ΠVx -= Ux1_2isom * (SUdAVx + SUdAVx')
-    ∂ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(1.0 ./Sx_2) - Ux1_2isom * UdAVx'* Diagonal(1.0 ./Sx_2)
-    _, convVx = cg!(vec(∂ΠVx),
-                    LinearMap{ElType}(∂w -> begin
-                                          ∂Π = reshape(∂w, (χ2^2, χc))
-                                          vec(∂Π*Diagonal(Sx_2.^2) - spmm(Tx', spmm(Tx, ∂Π)) + VS2x*Ux1_2isom'∂Π)
-                                      end, nothing, χ2^2*χc, χ2^2*χc),
-                    vec(∂LHS_ΠVx), log=true)
-    ∂Ux1_2iso += ∂ΠVx
+    begin
+        local Ux0_2isom = reshape(Ux0_2isom, (χ2^2, χc))
+        local Ux1_2isom = reshape(Ux1_2isom, (χ2^2, χc))
+        local UdAVx = Ux0_2isom'spmm(∂Tx, Ux1_2isom)
+        local Fij, UdAVSx, SUdAVx, US2x, VS2x, ∂LHS_ΓUx, ∂ΓUx, ∂LHS_ΠVx, ∂ΠVx
+        ∂Sx = diag(UdAVx)
+        Fij = [if i == j
+                   0.0
+               else
+                   1.0 / (Sx_2[j]^2 - Sx_2[i]^2)
+               end for i=1:χc, j=1:χc]
+        UdAVSx = UdAVx * Diagonal(Sx_2)
+        SUdAVx = Diagonal(Sx_2) * UdAVx
+        ∂Ux0_2iso = Ux0_2isom * (Fij .*(UdAVSx + UdAVSx'))
+        ∂Ux1_2iso = Ux1_2isom * (Fij .*(SUdAVx + SUdAVx'))
+        US2x = Ux0_2isom * Diagonal(Sx_2.^2)
+        VS2x = Ux1_2isom * Diagonal(Sx_2.^2)
+        # U off-diagonal part.
+        # LHS
+        ∂LHS_ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(Sx_2) + spmm(Tx, spmm(∂Tx', Ux0_2isom))
+        ∂LHS_ΓUx -= Ux0_2isom * (UdAVSx + UdAVSx')
+        # Trial: solution at U⊥'dAV⊥ = 0.
+        ∂ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(1.0 ./Sx_2) - Ux0_2isom * UdAVx * Diagonal(1.0 ./Sx_2)
+        _, convUx = cg!(vec(∂ΓUx),
+                        LinearMap{ElType}(∂v -> begin
+                                              ∂Γ = reshape(∂v, (χ2^2, χc))
+                                              vec(∂Γ*Diagonal(Sx_2.^2) - spmm(Tx, spmm(Tx', ∂Γ)) + US2x*Ux0_2isom'∂Γ)
+                                          end, nothing, χ2^2*χc, χ2^2*χc),
+                        vec(∂LHS_ΓUx), log=true)
+        ∂Ux0_2iso += ∂ΓUx
+        # V off-diagonal part.
+        ∂LHS_ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(Sx_2) + spmm(Tx', spmm(∂Tx, Ux1_2isom))
+        ∂LHS_ΠVx -= Ux1_2isom * (SUdAVx + SUdAVx')
+        ∂ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(1.0 ./Sx_2) - Ux1_2isom * UdAVx'* Diagonal(1.0 ./Sx_2)
+        _, convVx = cg!(vec(∂ΠVx),
+                        LinearMap{ElType}(∂w -> begin
+                                              ∂Π = reshape(∂w, (χ2^2, χc))
+                                              vec(∂Π*Diagonal(Sx_2.^2) - spmm(Tx', spmm(Tx, ∂Π)) + VS2x*Ux1_2isom'∂Π)
+                                          end, nothing, χ2^2*χc, χ2^2*χc),
+                        vec(∂LHS_ΠVx), log=true)
+        ∂Ux1_2iso += ∂ΠVx
+    end
+
+    # Uy-part, diagonal part.
+    begin
+        local Uy0_2isom = reshape(Uy0_2isom, (χ2^2, χc))
+        local Uy1_2isom = reshape(Uy1_2isom, (χ2^2, χc))
+        local UdAVy = Uy0_2isom'spmm(∂Ty, Uy1_2isom)
+        local Fij, UdAVSy, SUdAVy, US2y, VS2y, ∂LHS_ΓUy, ∂ΓUy, ∂LHS_ΠVy, ∂ΠVy
+        ∂Sy = diag(UdAVy)
+        Fij = [if i == j
+                   0.0
+               else
+                   1.0 / (Sy_2[j]^2 - Sy_2[i]^2)
+               end for i=1:χc, j=1:χc]
+        UdAVSy = UdAVy * Diagonal(Sy_2)
+        SUdAVy = Diagonal(Sy_2) * UdAVy
+        ∂Uy0_2iso = Uy0_2isom * (Fij .*(UdAVSy + UdAVSy'))
+        ∂Uy1_2iso = Uy1_2isom * (Fij .*(SUdAVy + SUdAVy'))
+        US2y = Uy0_2isom * Diagonal(Sy_2.^2)
+        VS2y = Uy1_2isom * Diagonal(Sy_2.^2)
+        # U off-diagonal part.
+        ∂LHS_ΓUy = spmm(∂Ty, Uy1_2isom) * Diagonal(Sy_2) + spmm(Ty, spmm(∂Ty', Uy0_2isom))
+        ∂LHS_ΓUy -= Uy0_2isom * (UdAVSy + UdAVSy')
+        ∂ΓUy = spmm(∂Ty, Uy1_2isom) * Diagonal(1.0 ./Sy_2) - Uy0_2isom * UdAVy * Diagonal(1.0 ./Sy_2)
+        _, convUy = cg!(vec(∂ΓUy),
+                        LinearMap{ElType}(∂v -> begin
+                                              ∂Γ = reshape(∂v, (χ2^2, χc))
+                                              vec(∂Γ*Diagonal(Sy_2.^2) - spmm(Ty, spmm(Ty', ∂Γ)) + US2y*Uy0_2isom'∂Γ)
+                                          end, nothing, χ2^2*χc, χ2^2*χc),
+                        vec(∂LHS_ΓUy), log=true)
+        ∂Uy0_2iso += ∂ΓUy
+        # V off-diagonal part.
+        ∂LHS_ΠVy = spmm(∂Ty', Uy0_2isom) * Diagonal(Sy_2) + spmm(Ty', spmm(∂Ty, Uy1_2isom))
+        ∂LHS_ΠVy -= Uy1_2isom * (SUdAVy + SUdAVy')
+        ∂ΠVy = spmm(∂Ty', Uy0_2isom) * Diagonal(1.0 ./Sy_2) - Uy1_2isom * UdAVy'* Diagonal(1.0 ./Sy_2)
+        _, convVy = cg!(vec(∂ΠVy),
+                        LinearMap{ElType}(∂w -> begin
+                                              ∂Π = reshape(∂w, (χ2^2, χc))
+                                              vec(∂Π*Diagonal(Sy_2.^2) - spmm(Ty', spmm(Ty, ∂Π)) + VS2y*Uy1_2isom'∂Π)
+                                          end, nothing, χ2^2*χc, χ2^2*χc),
+                        vec(∂LHS_ΠVy), log=true)
+        ∂Uy1_2iso += ∂ΠVy
+    end
 
 end
 
--- test/svds_derivative.jl
+++ test/svds_derivative.jl
@@ -24,44 +24,49 @@ begin
     spmm(A, B) = Array(A * B)
 
     # Ux-part, diagonal part.
-    Ux0_2isom = reshape(Ux0_2isom, (χ2^2, χc))
-    Ux1_2isom = reshape(Ux1_2isom, (χ2^2, χc))
-    UdAVx = Ux0_2isom'spmm(∂Tx, Ux1_2isom)
-    ∂Sx = diag(UdAVx)
-    Fij = [if i == j
-               0.0
-           else
-               1.0 / (Sx_2[j]^2 - Sx_2[i]^2)
-           end for i=1:χc, j=1:χc]
-    UdAVSx = UdAVx * Diagonal(Sx_2)
-    SUdAVx = Diagonal(Sx_2) * UdAVx
-    ∂Ux0_2iso = Ux0_2isom * (Fij .*(UdAVSx + UdAVSx'))
-    ∂Ux1_2iso = Ux1_2isom * (Fij .*(SUdAVx + SUdAVx'))
-    US2x = Ux0_2isom * Diagonal(Sx_2.^2)
-    VS2x = Ux1_2isom * Diagonal(Sx_2.^2)
-    # U off-diagonal part.
-    # LHS
-    ∂LHS_ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(Sx_2) + spmm(Tx, spmm(∂Tx', Ux0_2isom))
-    ∂LHS_ΓUx -= Ux0_2isom * (UdAVSx + UdAVSx')
-    # Trial: solution at U⊥'dAV⊥ = 0.
-    ∂ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(1.0 ./Sx_2) - Ux0_2isom * UdAVx * Diagonal(1.0 ./Sx_2)
-    _, convUx = cg!(vec(∂ΓUx),
-                    LinearMap{ElType}(∂v -> begin
-                                          ∂Γ = reshape(∂v, (χ2^2, χc))
-                                          vec(∂Γ*Diagonal(Sx_2.^2) - spmm(Tx, spmm(Tx', ∂Γ)) + US2x*Ux0_2isom'∂Γ)
-                                      end, nothing, χ2^2*χc, χ2^2*χc),
-                    vec(∂LHS_ΓUx), log=true)
-    ∂Ux0_2iso += ∂ΓUx
-    # V off-diagonal part.
-    ∂LHS_ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(Sx_2) + spmm(Tx', spmm(∂Tx, Ux1_2isom))
-    ∂LHS_ΠVx -= Ux1_2isom * (SUdAVx + SUdAVx')
-    ∂ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(1.0 ./Sx_2) - Ux1_2isom * UdAVx'* Diagonal(1.0 ./Sx_2)
-    _, convVx = cg!(vec(∂ΠVx),
-                    LinearMap{ElType}(∂w -> begin
-                                          ∂Π = reshape(∂w, (χ2^2, χc))
-                                          vec(∂Π*Diagonal(Sx_2.^2) - spmm(Tx', spmm(Tx, ∂Π)) + VS2x*Ux1_2isom'∂Π)
-                                      end, nothing, χ2^2*χc, χ2^2*χc),
-                    vec(∂LHS_ΠVx), log=true)
-    ∂Ux1_2iso += ∂ΠVx
+    begin
+        local Ux0_2isom = reshape(Ux0_2isom, (χ2^2, χc))
+        local Ux1_2isom = reshape(Ux1_2isom, (χ2^2, χc))
+        local UdAVx = Ux0_2isom'spmm(∂Tx, Ux1_2isom)
+        local Fij, UdAVSx, SUdAVx, US2x, VS2x, ∂LHS_ΓUx, ∂ΓUx, ∂LHS_ΠVx, ∂ΠVx
+        ∂Sx = diag(UdAVx)
+        Fij = [if i == j
+                   0.0
+               else
+                   1.0 / (Sx_2[j]^2 - Sx_2[i]^2)
+               end for i=1:χc, j=1:χc]
+        UdAVSx = UdAVx * Diagonal(Sx_2)
+        SUdAVx = Diagonal(Sx_2) * UdAVx
+        ∂Ux0_2iso = Ux0_2isom * (Fij .*(UdAVSx + UdAVSx'))
+        ∂Ux1_2iso = Ux1_2isom * (Fij .*(SUdAVx + SUdAVx'))
+        US2x = Ux0_2isom * Diagonal(Sx_2.^2)
+        VS2x = Ux1_2isom * Diagonal(Sx_2.^2)
+        # U off-diagonal part.
+        # LHS
+        ∂LHS_ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(Sx_2) + spmm(Tx, spmm(∂Tx', Ux0_2isom))
+        ∂LHS_ΓUx -= Ux0_2isom * (UdAVSx + UdAVSx')
+        # Trial: solution at U⊥'dAV⊥ = 0.
+        ∂ΓUx = spmm(∂Tx, Ux1_2isom) * Diagonal(1.0 ./Sx_2) - Ux0_2isom * UdAVx * Diagonal(1.0 ./Sx_2)
+        _, convUx = cg!(vec(∂ΓUx),
+                        LinearMap{ElType}(∂v -> begin
+                                              ∂Γ = reshape(∂v, (χ2^2, χc))
+                                              vec(∂Γ*Diagonal(Sx_2.^2) - spmm(Tx, spmm(Tx', ∂Γ)) + US2x*Ux0_2isom'∂Γ)
+                                          end, nothing, χ2^2*χc, χ2^2*χc),
+                        vec(∂LHS_ΓUx), log=true)
+        ∂Ux0_2iso += ∂ΓUx
+        # V off-diagonal part.
+        ∂LHS_ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(Sx_2) + spmm(Tx', spmm(∂Tx, Ux1_2isom))
+        ∂LHS_ΠVx -= Ux1_2isom * (SUdAVx + SUdAVx')
+        ∂ΠVx = spmm(∂Tx', Ux0_2isom) * Diagonal(1.0 ./Sx_2) - Ux1_2isom * UdAVx'* Diagonal(1.0 ./Sx_2)
+        _, convVx = cg!(vec(∂ΠVx),
+                        LinearMap{ElType}(∂w -> begin
+                                              ∂Π = reshape(∂w, (χ2^2, χc))
+                                              vec(∂Π*Diagonal(Sx_2.^2) - spmm(Tx', spmm(Tx, ∂Π)) + VS2x*Ux1_2isom'∂Π)
+                                          end, nothing, χ2^2*χc, χ2^2*χc),
+                        vec(∂LHS_ΠVx), log=true)
+        ∂Ux1_2iso += ∂ΠVx
+    end
+
+    ∂Ux0_2iso, ∂Sx, ∂Ux1_2iso
 end
 
