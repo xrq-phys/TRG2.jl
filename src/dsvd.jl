16ecb5a3a5e4e2b6b3f28dabb26fbbe935bd47be
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-11-07 03:07:56 +0900

    Separate ∂svds - Not Refactored Yet

---
+++ src/dsvd.jl
@@ -0,0 +1,46 @@
+# Derivative of full SVD.
+#  small-size fallback & reference.
+#
+using LinearAlgebra
+
+∂svd(A::Matrix{T},
+     dA::Matrix{T}) where {T} = begin
+    # SVD for values of A.
+    U, s, V = svd(A)
+
+    ∂svd(A, dA, U, s, V)
+end
+
+∂svd(A::Matrix{T},
+     dA::Matrix{T},
+     U::AbstractMatrix{T},
+     s::AbstractVector{T},
+     V::AbstractMatrix{T}) where {T} = begin
+    # bond dimensions
+    χo, χi = size(A)
+    # 'rank' of matrix
+    r = length(s)
+    size(U) == (χo, r) || throw(DimensionMismatch("U has incorrect size."))
+    size(V) == (χi, r) || throw(DimensionMismatch("V has incorrect size."))
+
+    # F matrix.
+    S = Diagonal(s)
+    Scol = repeat(s, outer=(1, r))
+    F = safercp.(Scol'.^2 - Scol.^2)
+
+    ds = diag(U' * dA * V)
+    dU = U * (F .* (U' * dA * V * S + S * V' * dA' * U)) + (I - U * U') * dA * V * safercp.(S)
+    dV = V * (F .* (S * U' * dA * V + V' * dA' * U * S)) + (I - V * V') * dA'* U * safercp.(S)
+
+    U, dU, s, ds, V, dV
+end
+
+safercp(x, c=0.0) = begin
+    # In fact this operation will sometimes cause big error.
+    if abs(x) < 1e-12
+        return x / (x^2 + 1e-12) + c
+    else
+        return 1. / x
+    end
+end
+
---
+++ src/dsvds.jl
@@ -0,0 +1,79 @@
+# Derivative of partial SVD.
+#  will be used to refactor derivative.jl.
+#
+using LinearAlgebra
+using LinearMaps
+using IterativeSolvers
+using Arpack
+
+SupportedMap{T} = Union{Matrix{T}, LinearMap{T}}
+
+∂svds(A ::SupportedMap{ElType},
+      ∂A::SupportedMap{ElType}, χc) where {ElType<:Number} = begin
+    # Compute partial SVD before differentiating.
+    (U, Sx, V), = svds(A, nsv=χc)
+
+    ∂svds(A, ∂A, U, Sx, V)
+end
+
+∂svds(A ::SupportedMap{ElType},
+      ∂A::SupportedMap{ElType},
+      U ::AbstractMatrix{ElType},
+      Sx::AbstractVector{ElType},
+      V ::AbstractMatrix{ElType}) where {ElType<:Number} = begin
+    local χc = length(Sx)
+    local χo, χi = size(A)
+    size(U) == (χo, χc) || throw(DimensionMismatch("U has incorrect size."))
+    size(V) == (χi, χc) || throw(DimensionMismatch("V has incorrect size."))
+
+    # SpGE→GE multiplication
+    spmm(A, B) = Array(A * B)
+
+    local UdAV = U'spmm(∂A, V)
+    local Fij
+    local UdAVS, US2, ∂LHS_ΓU, ∂ΓU
+    local SUdAV, VS2, ∂LHS_ΠV, ∂ΠV
+    ∂Sx = diag(UdAV)
+    Fij = [if i == j
+               0.0
+           else
+               1.0 / (Sx[j]^2 - Sx[i]^2)
+           end for i=1:χc, j=1:χc]
+    UdAVS = UdAV * Diagonal(Sx)
+    SUdAV = Diagonal(Sx) * UdAV
+    ∂U = U * (Fij .*(UdAVS + UdAVS'))
+    ∂V = V * (Fij .*(SUdAV + SUdAV'))
+    US2 = U * Diagonal(Sx.^2)
+    VS2 = V * Diagonal(Sx.^2)
+
+    # U off-diagonal part.
+    # LHS
+    ∂LHS_ΓU = spmm(∂A, V) * Diagonal(Sx) + spmm(A, spmm(∂A', U))
+    ∂LHS_ΓU -= U * (UdAVS + UdAVS')
+    # Trial: solution at U⊥'dAV⊥ = 0.
+    ∂ΓU = spmm(∂A, V) * Diagonal(1.0 ./Sx) - U * UdAV * Diagonal(1.0 ./Sx)
+    _, convUx = cg!(vec(∂ΓU),
+                    LinearMap{ElType}(∂v -> begin
+                                          ∂Γ = reshape(∂v, (χo, χc))
+                                          vec(∂Γ*Diagonal(Sx.^2) - spmm(A, spmm(A', ∂Γ)) + US2*U'∂Γ)
+                                      end, nothing, χo*χc, χo*χc),
+                    vec(∂LHS_ΓU), log=true)
+    @show convUx
+    ∂U += ∂ΓU
+
+    # V off-diagonal part.
+    ∂LHS_ΠV = spmm(∂A', U) * Diagonal(Sx) + spmm(A', spmm(∂A, V))
+    ∂LHS_ΠV -= V * (SUdAV + SUdAV')
+    ∂ΠV = spmm(∂A', U) * Diagonal(1.0 ./Sx) - V * UdAV'* Diagonal(1.0 ./Sx)
+    _, convVx = cg!(vec(∂ΠV),
+                    LinearMap{ElType}(∂w -> begin
+                                          ∂Π = reshape(∂w, (χi, χc))
+                                          vec(∂Π*Diagonal(Sx.^2) - spmm(A', spmm(A, ∂Π)) + VS2*V'∂Π)
+                                      end, nothing, χi*χc, χi*χc),
+                    vec(∂LHS_ΠV), log=true)
+    @show convVx
+    ∂V += ∂ΠV
+
+    U, ∂U, Sx, ∂Sx, V, ∂V
+end
+
